3d085c3abbfcf9ce5918b2e11a56c579a07303e8
szatolokinweb <szatolokin.web@gmail.com>
2022-07-17 02:01:58 +0300

    Add Control

--- src/components/Control/Control.js
+++ src/components/Control/Control.js
@@ -1,7 +1,31 @@
-export const Control = ({ children }) => {
+import { useCallback } from "react";
+
+import styles from "./Control.module.scss";
+
+import { Button } from "../Button/Button";
+
+export const Control = ({ value, onChange, min, max }) => {
   return (
-    <div>
-      <button></button>
+    <div className={styles.grid}>
+      <div>
+        <Button
+          full
+          onClick={() => value - 1 >= min && onChange(value - 1)}
+          disabled={value === min}
+        >
+          -
+        </Button>
+      </div>
+      <div className={styles.value}>{value}</div>
+      <div>
+        <Button
+          full
+          onClick={() => value + 1 <= max && onChange(value + 1)}
+          disabled={value === max}
+        >
+          +
+        </Button>
+      </div>
     </div>
   );
 };
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,19 +1,34 @@
+import { useState } from "react";
+
 import styles from "./Menu.module.scss";
 
 import { Button } from "../Button/Button";
+import { Control } from "../Control/Control";
 
 export const Menu = () => {
+  const [width, setWidth] = useState(3);
+  const [height, setHeight] = useState(5);
+  const [stepsCount, setStepsCount] = useState(10);
+
   return (
     <div className={styles.menu}>
       <div>
         <div>
-          <Button theme="danger">123</Button>
+          <div className={styles.label}>Ширина:</div>
+          <Control value={width} onChange={setWidth} min={3} max={10} />
         </div>
         <div>
-          <Button full>123</Button>
+          <div className={styles.label}>Высота:</div>
+          <Control value={height} onChange={setHeight} min={3} max={10} />
         </div>
         <div>
-          <Button disabled>123</Button>
+          <div className={styles.label}>Кол-во шагов:</div>
+          <Control
+            value={stepsCount}
+            onChange={setStepsCount}
+            min={5}
+            max={20}
+          />
         </div>
       </div>
       <div>
